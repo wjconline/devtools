f2ed3d62aecd1fa230d6ba9fefdf0f83cebc196f
Wesley C <wjconline@gmail.com>
2021-07-15 13:23:32 -0600

    develop - local node server; a11y contrast test page - wjconline - 210715

---
+++ node/localserver/localserver.js
@@ -0,0 +1,64 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function (request, response) {
+    console.log('request ', request.url);
+
+    // const rootPath = '.';
+    // const rootPath = '../..';
+    const rootPath = '/data/code/098_devtools_github/devtools';
+    let filePath = rootPath + request.url;
+    const siteIndexPath = rootPath + '/index.html';
+    if (request.url == '/') {
+    // if (filePath == './') {
+    // if (filePath == './../../') {
+        // filePath = '/testindex.html';
+        filePath = siteIndexPath;
+    }
+    console.log('filePath ', filePath);
+
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.svg': 'image/svg+xml',
+        '.wav': 'audio/wav',
+        '.ico': 'image/x-icon',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.wasm': 'application/wasm'
+    };
+
+    let contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            console.log('error ', error);
+            if(error.code == 'ENOENT') {
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(404, { 'Content-Type': 'text/html' });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(1337);
+console.log('Server running at http://127.0.0.1:1337/');
